654637d35da4d4c517f3162abdc612403d49f9a4
BramAlexander <83816999+BramAlexander@users.noreply.github.com>
2022-02-28 12:52:31 +0000

    Add files via upload

---
+++ sirSpecDE.m
@@ -0,0 +1,54 @@
+function dydt = sirSpecDE(~,y,params,mainNode,neighbourNode)
+
+% Note:
+% (mainNode,neighbourNode) corresponds to the (i,j) coefficient of one of
+% the 1s in the adjacency matrix. They are used in treeModelRates and
+% classicModelRates in order to avoid doing too many unnecessary
+% calculations of things that will be multiplied by zero.
+
+%% Preliminaries
+
+% Parameter extraction
+numNodes = numel(y)/2;
+lambda = params(1);
+gamma = params(2);
+alpha = params(3);
+
+% Initialisation
+dydt = zeros(size(y));
+treeModelRates = zeros(numNodes);
+classicModelRates = zeros(numNodes);
+s = y(1:numNodes);
+i = y((numNodes+1):2*numNodes);
+
+%% Calculate rates
+
+% In order to calculate the rate of infection at each node, we construct
+% matrices (initialised above) where the (i,j) element corresponds to the
+% rate of infection at i due to a neighbour at j. These matrices are only
+% nonzero where there is an edge between i and j, and so the calculations
+% are only done where [i,j] = [mainNode,neighbourNode], based on the
+% nonzero elements of the adjacency matrix.
+
+% adjIndex is the index (in single number form) of the nonzero elements of
+% the adjancency matrix.
+adjIndex = (neighbourNode-1)*numNodes+mainNode;
+
+% Infection rate according to the model that is exact for trees.
+treeModelRates(adjIndex) = ...
+    max((lambda+gamma)*s(mainNode) - lambda*s(neighbourNode) - gamma,0);
+
+% Infection rate based on treating nodes as independent.
+classicModelRates(adjIndex) = ...
+    lambda*s(mainNode).*i(neighbourNode);
+
+% Infection rate from a weighted average of the two models.
+totalInfRate = (1-alpha)*sum(treeModelRates,2) ...
+    + alpha*sum(classicModelRates,2);
+
+% Rate of change of susceptible probability is -totalInfRate.
+% Rate of change of infected probability is +totalInfRate - recoveryRate.
+dydt(1:numNodes) = -totalInfRate;
+dydt((numNodes+1):2*numNodes) = totalInfRate - gamma*i;
+
+end
